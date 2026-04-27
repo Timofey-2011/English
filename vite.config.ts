c31e5adfd4d15045bef51f5777e7ce7e62e750a3
Timofey-2011 <timatlanov@gmail.com>
2026-04-27 21:09:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/English/',
+  base: './', // Точка и слэш заставляют пути быть относительными
+  build: {
+    outDir: 'dist',
+  }
 })
